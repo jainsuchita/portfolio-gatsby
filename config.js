fb3f4a20baa2559ef79f74fcc4abff4b17960f05
jainsuchi21 <jainsuchi21@gmail.com>
2019-07-01 19:00:26 +0530

    Add config file

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    siteTitle: 'Suchita Jain', // <title>,
+    siteDescription: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    manifestName: `gatsby-starter-default`,
+    manifestShortName: `starter`, // max 12 characters
+    manifestStartUrl: `/`,
+    manifestBackgroundColor: `#663399`,
+    manifestThemeColor: `#663399`,
+    manifestDisplay: `minimal-ui`,
+    manifestIcon: `src/images/gatsby-icon.png`,
+    pathPrefix: `/gatsby-starter-default/`, // This path is subpath of your hosting https://domain/portfolio
+    authorName: 'Suchita',
+    heading: 'Web Developer',
+    // social
+    socialLinks: [
+        {
+            icon: 'fa-github',
+            name: 'Github',
+            url: 'http://github.com/jainsuchita',
+        },
+        {
+            icon: 'fa-instagram',
+            name: 'Instagram',
+            url: 'https://www.instagram.com/suchita.404error/',
+        },
+        {
+            icon: 'fa-envelope-o',
+            name: 'Email',
+            url: 'mailto:jainsuchi21@gmail.com',
+        },
+    ],
+};
